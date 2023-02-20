affa661d253423424e60992b7ad073865770304e
Jolan Stgj <94522110+Shokay@users.noreply.github.com>
2023-02-20 18:53:35 +0100

    TimerInLastScene

--- Assets/StartEndScene.cs
+++ Assets/StartEndScene.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartEndScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Timer timer;
 
-    // Update is called once per frame
-    void Update()
+        private void OnTriggerEnter(Collider other)
     {
-        
+
+        if (other.GetComponent<CapsuleCollider>() != null)
+        {
+            timer.GetTimeSaved();
+           SceneManager.LoadScene("EndScreen");
+        }
     }
 }
+
