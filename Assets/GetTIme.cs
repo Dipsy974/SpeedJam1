fb235dce34e5b8ba1d6cc266e6077fad3b2beac8
DESKTOP-GH0KFUH\Quentin <q.izadpanah@rns.ecolescreatives.com>
2023-02-20 19:34:02 +0100

    Push Final

--- Assets/GetTIme.cs
+++ Assets/GetTIme.cs
@@ -6,7 +6,7 @@ using TMPro;
 public class GetTIme : MonoBehaviour
 {
 
-    public TextMeshProUGUI timerText;
+    public TextMesh timerText;
 
     // Start is called before the first frame update
     void Start()
